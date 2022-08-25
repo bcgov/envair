235fc671524071be29df6e9bb5df5d74cad6decd
Robles <Jerome.Robles@gov.bc.ca>
2022-08-25 13:16:18 -0700

    added air zones in listBC_stations()

--- R/listBC_stations.R
+++ R/listBC_stations.R
@@ -1,11 +1,11 @@
 # Copyright 2022 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
@@ -150,6 +150,26 @@ listBC_stations<-function(year=NULL)
 
   # file.remove(file.temp)   #delete the temporary file
   station.details <- tibble::as_tibble(station.details)
+
+  #retrieve air zone details
+
+  try(
+    station.details <- station.details %>%
+      dplyr::select(-AIRZONE)
+  )
+
+  airzone_details <-
+    station.details %>%
+    dplyr::mutate(lat = as.numeric(LAT), lon = as.numeric(LONG), ems_id = EMS_ID) %>%
+    filter(!is.na(lat),!is.na(lon),abs(lat)<=90) %>%
+    rcaaqs::assign_airzone(bcmaps::airzones()) %>%
+    dplyr::rename(AIRZONE = airzone) %>%
+    dplyr::select(LAT,LONG,AIRZONE) %>%
+    unique()
+
+  station.details <- station.details %>%
+    dplyr::left_join(airzone_details)
+
   return(station.details)
 
 }
