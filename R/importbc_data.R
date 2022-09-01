5fd04ce83d0ad2b4faafbe125671795e7acf0ab4
Robles <Jerome.Robles@gov.bc.ca>
2022-09-01 10:39:37 -0700

    addded importBC_data_avg() function

--- R/importbc_data.R
+++ R/importbc_data.R
@@ -78,6 +78,15 @@ importBC_data<-function(parameter_or_station,
   temp_ <- as.character(unlist(stringi::stri_split_lines(RCurl::getURL(data.unvalidated_source,dirlistonly=TRUE))))
   temp_<-gsub('.csv','',temp_,ignore.case=TRUE)
 
+  #process parameter or station
+  parameter_or_station <- tolower(parameter_or_station)
+  parameter_or_station <- gsub('ozone','o3',parameter_or_station)
+  parameter_or_station <- gsub('pm2.5','pm25',parameter_or_station)
+  parameter_or_station <- gsub('wdir','wdir_vect',parameter_or_station)
+  parameter_or_station <- gsub('wspd','wspd_sclr',parameter_or_station)
+  parameter_or_station <- gsub('rh','humidity',parameter_or_station)
+
+
   if (any(tolower(parameter_or_station) %in% tolower(temp_)))
   {
     parameters <- parameter_or_station
@@ -468,4 +477,268 @@ importBC_data<-function(parameter_or_station,
   return(data.result)
 }
 
+#' Average the imported data function
+#'
+#' This function averages the imported data
+#'
+#' @param parameter is the parameter to average.use list_parameters() for list of parameters
+#' @param years specifies the years to include. Use vector for multiple years. If NULL, it uses the current year.
+#' @param averaging_type is the sub-annual values of either "1-hour", "24-hour", "8-hour","d1hm", "d8hm". If NULL, it applies the parameter default
+#' @param data_threshold  value between 0 to 1. This the data capture requirements before reporting the average. Enter 0 if you want to include all values.
+#'
+#' @examples
+#' importBC_data_avg('o3')   #retrieves the daily 8-hour maximum for current year
+#' importBC_data_avg('o3')
+#'
+#' @export
+importBC_data_avg <- function(parameter, years = NULL, averaging_type =  NULL, data_threshold = 0.75)
+{
+  if (0) {
+    source('./r/importBC_data.R')
+    source('./r/listBC_stations.R')
+    source('./r/paddatafunction.R')
+    parameter <- 'OZONE'
+    years <- NULL
+    averaging_type <- NULL
+    data_threshold <- 0.75
+  }
 
+  require(dplyr)
+  #rename the parameter entries
+  parameter <- tolower(parameter)
+  parameter <- gsub('ozone','o3',parameter)
+  parameter <- gsub('pm2.5','pm25',parameter)
+  parameter <- gsub('wdir','wdir_vect',parameter)
+  parameter <- gsub('wspd','wspd_sclr',parameter)
+  parameter <- gsub('rh','humidity',parameter)
+
+  #define the default averaging type
+  df_defaults <- tibble::tribble(
+    ~parameter,~averaging_type,
+    'pm25','24-hour',
+    'o3','d8hm',
+    'so2','d1hm',
+    'no2','d1hm',
+    'pm10','24-hour'
+  )
+
+  lst_averaging_types <- c(
+    'd1hm','d8hm',
+    '24-hour','24hr','24-hr','24','day','24h','24 hr','24 h',
+    '1-hour','1hr','1-hr','1','hr','1h','hour',
+    '8hr','8-hr','8h','8-hour','8 hr','8 h'
+  )
+
+  #assigning default values
+  if (is.null(years)){
+    years <- lubridate::year(Sys.Date())
+  }
+
+  if (is.null(averaging_type)) {
+      averaging_type <- df_defaults$averaging_type[df_defaults$parameter == parameter]
+      if (length(averaging_type) ==0) {averaging_type <- '1-hour'}
+  }
+
+  averaging_type <- tolower(averaging_type)
+
+
+  #add extra year for d8hm
+  if (averaging_type == 'd8hm') {
+   years_ <-  c(min(years)-1,years)
+  } else {
+    years_ <- years
+  }
+  #retrieve data
+  df <- importBC_data(parameter = parameter,
+                      years = years_ )
+
+
+  #add datetime_ columns for time-beginning processing
+
+
+  if (averaging_type %in% c('1-hour','1hr','1-hr','1','hr','1h','hour'))
+  {
+
+    df <-   df %>%
+      pad_data()
+    return(df)
+  }
+
+  if (averaging_type %in% c('24-hour','24hr','24-hr','24','day','24h','24 hr','24 h'))
+  {
+    df <- df %>%
+      ungroup() %>%
+      filter(!is.na(RAW_VALUE)) %>%
+      group_by(DATE,STATION_NAME,INSTRUMENT,PARAMETER) %>%
+      dplyr::summarise(RAW_VALUE_24h = mean(RAW_VALUE),
+                       ROUNDED_VALUE_24h = mean(ROUNDED_VALUE),
+                       valid_hrs = n()) %>%
+      filter(valid_hrs>=data_threshold * 24) %>%
+      pad_data(date_time = 'DATE',padby='day',
+               values = c('RAW_VALUE_24h','ROUNDED_VALUE_24h','valid_hrs'))
+
+    if(data_threshold != 0) {
+      df <- df %>% dplyr::select(-valid_hrs)
+    }
+
+    return(df)
+
+  }
+
+  if (averaging_type %in% c('d1hm')){
+    df <- df %>%
+      ungroup() %>%
+      filter(!is.na(RAW_VALUE)) %>%
+      group_by(DATE,STATION_NAME,INSTRUMENT,PARAMETER) %>%
+      dplyr::summarise(RAW_VALUE_D1HM = max(RAW_VALUE),
+                       ROUNDED_VALUE_D1HM = max(ROUNDED_VALUE),
+                       valid_hrs = n()) %>%
+      filter(valid_hrs>=data_threshold * 24) %>%
+      pad_data(date_time = 'DATE',padby='day',
+               values = c('RAW_VALUE_D1HM','ROUNDED_VALUE_D1HM','valid_hrs'))
+
+    if(data_threshold != 0) {
+      df <- df %>% dplyr::select(-valid_hrs)
+    }
+
+    return(df)
+  }
+
+  if (averaging_type %in% c('d8hm')){
+    print('Calculating daily 8-hour maximums')
+    cols <- colnames(df)
+
+    #pre-defined column for grouping
+    cols_select <- c('STATION_NAME','INSTRUMENT','PARAMETER')
+
+    #check columns based on actuals
+    cols_select <- unique(cols_select[cols_select %in% cols])
+
+
+
+    #additional columns value and date columns
+    cols_select_all <- c(cols_select,'DATE_PST','RAW_VALUE','ROUNDED_VALUE')
+    cols_select_all <- unique(cols_select_all[cols_select_all %in% cols])
+
+    df_ <-  df %>%
+      ungroup() %>%
+        select(cols_select_all)
+
+    #duplicate previous 7 hours to group together
+    for (i in 1:7) {
+
+      print(paste('Calculating running average:',i,'/7',sep=''))
+      df_ <- df_ %>%
+        dplyr::bind_rows(
+          df %>%
+            dplyr::select(cols_select_all) %>%
+            dplyr::mutate(DATE_PST = DATE_PST + lubridate::hours(i))
+        )
+    }
+
+
+    #calculate running average
+    df <- df_ %>%
+      filter(!is.na(RAW_VALUE)) %>%
+      dplyr::group_by(dplyr::across(c('DATE_PST',cols_select))) %>%
+      dplyr::summarise(RAW_VALUE_8h = mean(RAW_VALUE),
+                    ROUNDED_VALUE_8h = mean(ROUNDED_VALUE),
+                    valid_n = n()) %>%
+      dplyr::filter(valid_n>=6) %>%  #6 hrs at least
+      dplyr::select(-valid_n)
+
+
+
+    # get the daily maximum
+    df <-  df %>%
+      ungroup() %>%
+      dplyr::mutate(datetime = DATE_PST-lubridate::hours(1)) %>%
+      dplyr::mutate(DATE = as.character(datetime,format='%Y-%m-%d')) %>%
+      dplyr::group_by(dplyr::across(c('DATE',cols_select))) %>%
+      dplyr::summarise(RAW_VALUE_D8HM = max(RAW_VALUE_8h),
+                       ROUNDED_VALUE_D8HM = max(ROUNDED_VALUE_8h),
+                       valid_hrs =n()
+      ) %>%
+      ungroup() %>%
+      dplyr::filter(valid_hrs>=data_threshold*24) %>%
+      dplyr::filter(lubridate::year(DATE) %in% years)
+
+    if (data_threshold != 0)  {
+      df <- df %>%
+        select(-valid_hrs)
+    }
+
+    df <- df %>%
+      pad_data(date_time = 'DATE',
+               values =c('RAW_VALUE_D8HM','ROUNDED_VALUE_D8HM','valid_hrs'),
+               padby = 'day')
+
+
+    return(df)
+
+  }
+
+  if (averaging_type %in% c('8hr','8-hr','8h','8-hour','8 hr','8 h')) {
+
+    print('Retrieving 8-hour running average')
+    cols <- colnames(df)
+
+    #pre-defined column for grouping
+    cols_select <- c('STATION_NAME','INSTRUMENT','PARAMETER')
+
+    #check columns based on actuals
+    cols_select <- unique(cols_select[cols_select %in% cols])
+
+
+
+    #additional columns value and date columns
+    cols_select_all <- c(cols_select,'DATE_PST','RAW_VALUE','ROUNDED_VALUE')
+    cols_select_all <- unique(cols_select_all[cols_select_all %in% cols])
+
+    df_ <-  df %>%
+      ungroup() %>%
+      select(cols_select_all)
+
+    #duplicate previous 7 hours to group together
+    for (i in 1:7) {
+
+      print(paste('Calculating running average:',i,'/7',sep=''))
+      df_ <- df_ %>%
+        dplyr::bind_rows(
+          df %>%
+            dplyr::select(cols_select_all) %>%
+            dplyr::mutate(DATE_PST = DATE_PST + lubridate::hours(i))
+        )
+    }
+
+
+    #calculate running average
+    df <- df_ %>%
+      filter(!is.na(RAW_VALUE)) %>%
+      dplyr::group_by(dplyr::across(c('DATE_PST',cols_select))) %>%
+      dplyr::summarise(RAW_VALUE_8h = mean(RAW_VALUE),
+                       ROUNDED_VALUE_8h = mean(ROUNDED_VALUE),
+                       valid_n = n()) %>%
+      dplyr::filter(valid_n>=data_threshold*8)
+
+
+    if (data_threshold != 0)  {
+      df <- df %>%
+        select(-valid_n)
+    }
+
+df <- df %>%
+   pad_data(values =c('RAW_VALUE_8h','ROUNDED_VALUE_8h','valid_n'),
+           padby = 'hour',add_DATETIME = TRUE)
+
+
+
+    return(df)
+  }
+
+  #function would have exit before this point
+
+  print(paste(averaging_type,'is not in the list of averaging types. Use d1hm, d8hm, 1-hour, or 24-hour'))
+  return(NULL)
+
+}
--- R/paddatafunction.R
+++ R/paddatafunction.R
@@ -15,17 +15,19 @@
 #' This function inserts fillers to data set. The function can identify separate columns of DATE and TIME
 #'
 #' @param df is the dataframe containing date_time parameters, and values columns
-#' @param date_time defines the date_time column. Default is DATE_PST
+#' @param date_time defines the date_time column. Default is DATE_PST. It can also be just dates if padby = 'day'
+#' @param padby is either "hour" or "day". This defines the increment of date_time column
 #' @param values defines the columns with the reported measurements. If NULL, it wil use RAW_VALUE and ROUNDED_VALUE
 #' @param time_ending if TRUE means date starts AFTER and EXCLUDES midnight
 #' @param add_DATETIME if TRUE, means to force and include DATETIME
 #'
 #' @export
-pad_data <- function(df, date_time = NULL,values = NULL,time_ending = TRUE,add_DATETIME = NULL)
+pad_data <- function(df,date_time = NULL,padby='hour' ,values = NULL,time_ending = TRUE,add_DATETIME = NULL)
 {
   if (0)  {
-    df <- readRDS('./test_data/raw_data.Rds')
 
+    df <- readRDS('./test_data/raw_data.Rds')
+    df <- readRDS('./test_data/raw_data_day.Rds')
      # saveRDS(test,'./test_data/raw_data.Rds')
     # df <- envair::importBC_data('no2',2020)
     # df <- data.result
@@ -33,12 +35,17 @@ pad_data <- function(df, date_time = NULL,values = NULL,time_ending = TRUE,add_D
     values <- NULL
     time_ending <- TRUE
     add_DATETIME = TRUE
-
+    padby <- 'day'
     df <- data.result
     date_time <-  c('date','DATE_PST')[c('date','DATE_PST') %in% cols_]
     values = c(cols_vals,cols_instrument,cols_unit)
     time_ending = !use_openairformat
     add_DATETIME = FALSE
+
+    date_time = 'DATE'
+    padby='day'
+    values = c('RAW_VALUE_D8HM','ROUNDED_VALUE_D8M','valid_hrs')
+
   }
 
   require(dplyr)
@@ -48,101 +55,147 @@ pad_data <- function(df, date_time = NULL,values = NULL,time_ending = TRUE,add_D
   df <- ungroup(df)
   cols_ <- colnames(df)
 
-  if (is.null(add_DATETIME)) {
-    # find out if separate DATE and TIME columns are included
-    separate_DATETIME <- all(c('DATE','TIME') %in% cols_)
-  } else {
-    separate_DATETIME <- add_DATETIME
-  }
-
-
-
-  # assign values for default entries
-  if (is.null(date_time)){
-    date_time <- cols_[cols_ %in% c('DATE_PST','datetime','date_time',
-                                    'date_pst','DATEPST')]
-    date_time <- date_time[1]
-  }
-  #check if special condition for importBC_data, where use_openair
-  #need to rename away from "date" as date_time column
-  if (date_time == 'date') {
-    special_openairformat <- TRUE
-    date_time <- 'date_pst'
-
-    add_DATETIME <- FALSE
-    separate_DATETIME <- FALSE
-    df <- df %>%
-      dplyr::rename(date_pst = date)
-    cols_ <- colnames(df)
-    time_ending <- FALSE
-
-  } else {
-    special_openairformat <- FALSE
-  }
-
-  if (is.null(values)) {
-    values <- cols_[cols_ %in% c('RAW_VALUE','ROUNDED_VALUE','metric_value','VALIDATION_STATUS','validation_status')]
-  }
-
-
-  cols_selection <- c(cols_[!cols_ %in% c(values,date_time)])
-
-  if (separate_DATETIME) {
-    cols_selection <- cols_selection[!cols_selection %in% c('DATE','TIME')]
-    try(df <- df %>%
-      dplyr::select(-DATE,-TIME), silent = TRUE)
-  }
-
-  #convert all date and time to time-beginning
-  if (time_ending) {
-    df[[date_time]] <- df[[date_time]] - lubridate::hours(1)
-
-  }
-
-  #special cse for use_openairformat
-
-  lst_datetime <- df %>%
-    pull(date_time)
-
-  start_date <- lubridate::ymd_hm(paste(lubridate::year(min(lst_datetime)),'-01-01 00:00',sep=''), tz='etc/GMT+8')
-  end_date <- lubridate::ymd_hm(paste(lubridate::year(max(lst_datetime)),'-12-31 23:00',sep=''), tz='etc/GMT+8')
-
-  df_datetime <- dplyr::tibble(!!date_time := seq.POSIXt(from = start_date, to=end_date , by='hour'))
-
-  if (separate_DATETIME) {
-    df_datetime$DATE <- lubridate::date(df_datetime[[date_time]])
-    df_datetime$TIME <- lubridate::hour(df_datetime[[date_time]])
-
-    if (time_ending)
-    {
-      df_datetime$TIME <- df_datetime$TIME +1
-      df_datetime$TIME <- paste(df_datetime$TIME,':00',sep='')
+  if (padby %in% c('hour','hours','1-hour','hr','1-hr','1h','hrs')) {
+    if (is.null(add_DATETIME)) {
+      # find out if separate DATE and TIME columns are included
+      separate_DATETIME <- all(c('DATE','TIME') %in% cols_)
+    } else {
+      separate_DATETIME <- add_DATETIME
     }
+
+    # assign values for default entries
+    if (is.null(date_time)){
+      date_time <- cols_[cols_ %in% c('DATE_PST','datetime','date_time',
+                                      'date_pst','DATEPST')]
+      date_time <- date_time[1]
+    }
+    #check if special condition for importBC_data, where use_openair
+    #need to rename away from "date" as date_time column
+    if (date_time == 'date') {
+      special_openairformat <- TRUE
+      date_time <- 'date_pst'
+
+      add_DATETIME <- FALSE
+      separate_DATETIME <- FALSE
+      df <- df %>%
+        dplyr::rename(date_pst = date)
+      cols_ <- colnames(df)
+      time_ending <- FALSE
+
+    } else {
+      special_openairformat <- FALSE
+    }
+
+    if (is.null(values)) {
+      values <- cols_[cols_ %in% c('RAW_VALUE','ROUNDED_VALUE','metric_value','VALIDATION_STATUS','validation_status')]
+    }
+
+
+    cols_selection <- c(cols_[!cols_ %in% c(values,date_time)])
+
+    if (separate_DATETIME) {
+      cols_selection <- cols_selection[!cols_selection %in% c('DATE','TIME')]
+      try(df <- df %>%
+            dplyr::select(-DATE,-TIME), silent = TRUE)
+    }
+
+    #convert all date and time to time-beginning
+    if (time_ending) {
+      df[[date_time]] <- df[[date_time]] - lubridate::hours(1)
+
+    }
+
+    #special cse for use_openairformat
+
+    lst_datetime <- df %>%
+      pull(date_time)
+
+    start_date <- lubridate::ymd_hm(paste(lubridate::year(min(lst_datetime)),'-01-01 00:00',sep=''), tz='etc/GMT+8')
+    end_date <- lubridate::ymd_hm(paste(lubridate::year(max(lst_datetime)),'-12-31 23:00',sep=''), tz='etc/GMT+8')
+
+    df_datetime <- dplyr::tibble(!!date_time := seq.POSIXt(from = start_date, to=end_date , by='hour'))
+
+    if (separate_DATETIME) {
+      df_datetime$DATE <- lubridate::date(df_datetime[[date_time]])
+      df_datetime$TIME <- lubridate::hour(df_datetime[[date_time]])
+
+      if (time_ending)
+      {
+        df_datetime$TIME <- df_datetime$TIME +1
+        df_datetime$TIME <- paste(df_datetime$TIME,':00',sep='')
+      }
+    }
+
+    df_result <- df_datetime %>%
+      merge(
+        df %>%
+          dplyr::select(cols_selection) %>%
+          unique()
+      ) %>%
+      dplyr::left_join(df)
+
+
+    #convert back the time if time ending
+    if (time_ending) {
+      df_result[[date_time]] <- df_result[[date_time]] + lubridate::hours(1)
+
+    }
+
+    #fix back for the special case scenario
+
+    if (special_openairformat) {
+
+      df_result <- df_result %>%
+        dplyr::rename(date = date_pst)
+    }
+
+    print(paste('Added/padded rows:',nrow(df_result) - nrow(df)))
   }
 
-  df_result <- df_datetime %>%
-    merge(
-      df %>%
-        dplyr::select(cols_selection) %>%
-        unique()
-    ) %>%
-    dplyr::left_join(df)
+
+  if (padby %in% c('day','days','24-hour','24hrs','24-hours','24h')) {
+
+    # assign values for default or NULL parameter entries
+    if (is.null(date_time)){
+      date_time <- cols_[cols_ %in% c('DATE','date')]
+      date_time <- date_time[1]
+    }
+
+    if (is.null(values))
+    {
+      values <- cols_[!cols_ %in% c(date_time,'STATION_NAME','INSTRUMENT','site','station','instrument',
+                                    'PARAMETER','parameter')]
+    }
+
+    df <- ungroup(df)
+    #change Date column to an Date datatype
+
+    df <- as.data.frame(df)
+    df[,date_time] <-  as.Date(df[,date_time])
 
 
-  #convert back the time if time ending
-  if (time_ending) {
-    df_result[[date_time]] <- df_result[[date_time]] + lubridate::hours(1)
+    cols_selection <- cols_[!(cols_ %in% c(values,date_time))]
 
+
+
+
+    lst_datetime <- df %>%
+      pull(date_time)
+
+    start_date <- lubridate::ymd(paste(lubridate::year(min(lst_datetime)),'-01-01',sep=''), tz='etc/GMT+8')
+    end_date <- lubridate::ymd(paste(lubridate::year(max(lst_datetime)),'-12-31',sep=''), tz='etc/GMT+8')
+
+    df_datetime <- dplyr::tibble(!!date_time := seq.Date(from = as.Date(start_date), to=as.Date(end_date) , by='day'))
+
+    df_result <- df_datetime %>%
+      merge(
+        df %>%
+          dplyr::select(cols_selection) %>%
+          unique()
+      ) %>%
+      dplyr::left_join(df)
+    print(paste('Added/padded rows:',nrow(df_result) - nrow(df)))
   }
 
-  #fix back for the special case scenario
-
-  if (special_openairformat) {
-
-    df_result <- df_result %>%
-      dplyr::rename(date = date_pst)
-  }
-
-  print(paste('Added/padded rows:',nrow(df_result) - nrow(df)))
   return(df_result)
 }
